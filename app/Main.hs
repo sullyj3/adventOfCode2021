a0e0dbe558814911500a1eb7b2ae867e2710ca06
James Sully <sullyj3@gmail.com>
2021-11-25 01:48:20 +1100

    Overengineered tree depth using NonDet and State

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,9 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = traverse_ (print . depth) [
+    (Node 1 [])
+  , (Node 1 [Node 2 []])
+  , (Node 1 [Node 2 [Node 3 []]])
+  , (Node 1 [Node 2 [Node 3 []], Node 4 []])
+  ]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,44 @@
+
 module Lib
-    ( someFunc
+    ( Tree(..)
+    , depth
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Polysemy
+import Polysemy.NonDet
+import Polysemy.State
+
+import Flow
+import Prelude hiding (State, evalState, get, modify)
+import Relude.Unsafe
+
+
+data Tree = Node Int [Tree]
+
+
+
+-- >>> depth (Node 1 [])
+-- 0
+-- >>> depth (Node 1 [Node 2 []])
+-- 1
+-- >>> depth (Node 1 [Node 2 [Node 3 []]])
+-- 2
+-- >>> depth (Node 1 [Node 2 [Node 3 []], Node 4 []])
+-- 2
+
+depth :: Tree -> Int
+depth t = depthSem t
+  |> runNonDet
+  |> evalState 0
+  |> run
+  |> maximum
+  where maximum :: [Int] -> Int
+        maximum = foldl' max minBound
+
+
+depthSem :: Tree -> Sem [NonDet, State Int] Int
+depthSem (Node _ []) = get
+depthSem (Node _ children) = do
+  modify (+1)
+  t <- asumMap pure children
+  depthSem t
