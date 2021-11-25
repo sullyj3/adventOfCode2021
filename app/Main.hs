601f758cdf3af77192060deb28331bfaa41465cd
James Sully <sullyj3@gmail.com>
2021-11-26 00:26:27 +1100

    Switch from stack to cabal
    stack repl (and therefore hls) doesn't support mixins

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,8 @@ import Lib
 
 main :: IO ()
 main = traverse_ (print . depth) [
-    (Node 1 [])
-  , (Node 1 [Node 2 []])
-  , (Node 1 [Node 2 [Node 3 []]])
-  , (Node 1 [Node 2 [Node 3 []], Node 4 []])
+    Node 1 []
+  , Node 1 [Node 2 []]
+  , Node 1 [Node 2 [Node 3 []]]
+  , Node 1 [Node 2 [Node 3 []], Node 4 []]
   ]
