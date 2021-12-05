3859de678d43cad622c342d817ad7fdde1b6cf92
James Sully <sullyj3@gmail.com>
2021-12-05 20:13:53 +1100

    Day 4 part 1

--- src/Day04.hs
+++ src/Day04.hs
@@ -6,14 +6,13 @@ import Text.Megaparsec.Char (newline, space)
 import Utils (showSolutions)
 import Text.Megaparsec.Char.Lexer (decimal)
 import Prelude hiding (many)
-import Data.Massiv.Array (Matrix, B)
-import Data.Massiv.Array qualified as MSV
+import Control.Monad (foldM)
 
-type Board = Matrix B Int
+type Board a = [[a]]
 
 type Parser = Parsec Void Text
 
-parseInput :: Parser ([Int], [Board])
+parseInput :: Parser ([Int], [Board Int])
 parseInput = do
   drawnNumbers <- decimal `sepBy` single ','
   space
@@ -21,27 +20,61 @@ parseInput = do
   eof
   pure (drawnNumbers, boards)
 
-parseBoard :: Parser Board
+{-
+debugging
+
+printBoard :: (Show a) => [[a]] -> IO ()
+printBoard b = traverse_ print b *> putStrLn ""
+
+printBoards = traverse_ printBoard
+
+exampleInput :: IO ([Int], [Board (Maybe Int)])
+exampleInput = do
+  t <- readFileText "inputs/day04example.txt"
+  let Just (ns, boards) = parseMaybe parseInput t
+  pure (ns, (map . map . map) Just boards)
+-}
+
+parseBoard :: Parser (Board Int)
 parseBoard = do
   row1 <- parseRow
   rows <- replicateM 4 (newline *> parseRow)
-  pure $ MSV.fromLists' MSV.Seq (row1:rows) 
+  pure (row1:rows)
 
 parseRow :: Parser [Int]
 parseRow = replicateM 5 (space *> decimal)
 
 solve :: Text -> Text
-solve input = showSolutions drawnNumbers boards
-  where Right (drawnNumbers, boards) = parse parseInput "day04.txt" input
-        p1 = ()
+solve input = showSolutions finalScore p2
+
+  where boards :: [Board Int]
+        Right (drawnNumbers, boards) = parse parseInput "day04.txt" input
+
+        finalScore :: Score
+        Left finalScore = foldM crossOutOnAllBoards ((map . map . map) Just boards) drawnNumbers
+
+
         p2 = ()
 
-oneBoard :: Text
-oneBoard = unlines 
-  [ "68 22 95 71 42"
-  , "18  0 61 86 44"
-  , "46 29 51 23 99"
-  , "11 60 66 12 52"
-  , "53 87 41 90 49"
-  ]
+type Score = Int
 
+crossOutOnAllBoards :: [Board (Maybe Int)] -> Int -> Either Score [Board (Maybe Int)]
+crossOutOnAllBoards boards' drawn = traverse (crossOut drawn) boards'
+
+crossOut :: Int -> Board (Maybe Int) -> Either Score (Board (Maybe Int))
+crossOut lastCalled board
+  | isWin replaced = Left finalScore
+  | otherwise = Right replaced
+  where replaced = map (map replaceCalled) board
+        replaceCalled = \case
+          Just n | n == lastCalled -> Nothing
+          x -> x
+
+        finalScore = lastCalled * (sum . concatMap catMaybes $ replaced)
+
+
+-- >>> isWin $ replicate 5 [Right 1, Left 2, Right 1, Right 1, Right 1]
+isWin :: Board (Maybe Int) -> Bool
+isWin rows = rowWin || colWin
+  where rowWin = any (all isNothing) rows
+        colWin = any (all isNothing) (transpose rows)
