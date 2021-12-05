596741eefd4afbcb76484389f45881892e908cbc
James Sully <sullyj3@gmail.com>
2021-12-05 23:12:55 +1100

    Formatting day5

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,17 +1,20 @@
 module Day05 where
 
+import Data.Map qualified as Map
+import Linear.V2 (V2 (..))
+import Text.Megaparsec
+  ( MonadParsec (eof),
+    Parsec,
+    parseMaybe,
+    sepEndBy1,
+    single,
+  )
+import Text.Megaparsec.Char (newline, string)
+import Text.Megaparsec.Char.Lexer (decimal)
 import Utils (showSolutions)
 
-import Text.Megaparsec
-import Linear.V2
-import Text.Megaparsec.Char.Lexer (decimal)
-import Text.Megaparsec.Char (string, newline)
-
-import Data.Map qualified as Map
-
-
 data Vent = Vent (V2 Int) (V2 Int)
-  deriving Show
+  deriving (Show)
 
 type Parser = Parsec Void Text
 
@@ -26,26 +29,27 @@ parseInput = sepEndBy1 vent newline <* eof
 
 solve :: Text -> Text
 solve input = showSolutions (part1 vents) (part2 vents)
-  where Just vents = parseMaybe parseInput input
+  where
+    Just vents = parseMaybe parseInput input
 
 part1 :: [Vent] -> Int
 part1 vents = Map.size overlappingPoints
   where
     noDiags = filter isHorizontalOrVertical vents
     PC pointCounts = foldMap ventPointCounts noDiags
-    overlappingPoints = Map.filter (>=2) pointCounts
+    overlappingPoints = Map.filter (>= 2) pointCounts
 
 part2 :: [Vent] -> Int
 part2 vents = Map.size overlappingPoints
   where
     PC pointCounts = foldMap ventPointCounts vents
-    overlappingPoints = Map.filter (>=2) pointCounts
+    overlappingPoints = Map.filter (>= 2) pointCounts
 
 isHorizontalOrVertical :: Vent -> Bool
 isHorizontalOrVertical (Vent (V2 x1 y1) (V2 x2 y2)) = x1 == x2 || y1 == y2
 
 newtype PointCounter = PC (Map (V2 Int) (Sum Int))
-  deriving Show
+  deriving (Show)
 
 instance Semigroup PointCounter where
   PC m1 <> PC m2 = PC $ Map.unionWith (<>) m1 m2
@@ -70,17 +74,17 @@ pcSingleton v = PC $ Map.singleton v 1
 -- [V2 0 2,V2 1 1,V2 2 0]
 ventPoints :: Vent -> [V2 Int]
 ventPoints (Vent u v)
-  | lx == rx = let [smallY, bigY] = sort [ly, ry]
-                in [ V2 lx y | y <- [smallY .. bigY]]
-  | ly == ry = [ V2 x ly | x <- [lx .. rx]]
-  | ly <= ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 1   ) left
-  | ly >  ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 (-1)) left
+  | lx == rx =
+    let [smallY, bigY] = sort [ly, ry]
+     in [V2 lx y | y <- [smallY .. bigY]]
+  | ly == ry = [V2 x ly | x <- [lx .. rx]]
+  | ly <= ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 1) left
+  | ly > ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 (-1)) left
   | otherwise = error "vent is not vertical, horizontal, or 45 degree diagonal"
   where
-    [left@(V2 lx ly), V2 rx ry] = sort [u,v]
+    [left@(V2 lx ly), V2 rx ry] = sort [u, v]
 
     notRightOf (V2 x _) r = x <= r
-
 
 -- >>> ventPointCounts (Vent (V2 1 0) (V2 1 5))
 -- >>> ventPointCounts (Vent (V2 0 0) (V2 0 9))
@@ -95,7 +99,6 @@ ventPointCounts = foldMap pcSingleton . ventPoints
 testInput :: IO Text
 testInput = do
   readFileText "inputs/day05example.txt"
-
 
 testVents :: IO [Vent]
 testVents = do
