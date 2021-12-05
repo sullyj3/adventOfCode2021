866fd2cce9a81eda8e1101cf94e0a991c43b1a39
James Sully <sullyj3@gmail.com>
2021-12-05 23:10:54 +1100

    Day 5 part 2

--- src/Day05.hs
+++ src/Day05.hs
@@ -25,7 +25,7 @@ parseInput :: Parser [Vent]
 parseInput = sepEndBy1 vent newline <* eof
 
 solve :: Text -> Text
-solve input = showSolutions (part1 vents) ()
+solve input = showSolutions (part1 vents) (part2 vents)
   where Just vents = parseMaybe parseInput input
 
 part1 :: [Vent] -> Int
@@ -33,6 +33,12 @@ part1 vents = Map.size overlappingPoints
   where
     noDiags = filter isHorizontalOrVertical vents
     PC pointCounts = foldMap ventPointCounts noDiags
+    overlappingPoints = Map.filter (>=2) pointCounts
+
+part2 :: [Vent] -> Int
+part2 vents = Map.size overlappingPoints
+  where
+    PC pointCounts = foldMap ventPointCounts vents
     overlappingPoints = Map.filter (>=2) pointCounts
 
 isHorizontalOrVertical :: Vent -> Bool
@@ -51,17 +57,30 @@ pcSingleton :: V2 Int -> PointCounter
 pcSingleton v = PC $ Map.singleton v 1
 
 -- >>> ventPoints (Vent (V2 1 0) (V2 1 5))
--- >>> ventPoints (Vent (V2 0 0) (V2 0 9))
 -- [V2 1 0,V2 1 1,V2 1 2,V2 1 3,V2 1 4,V2 1 5]
+-- >>> ventPoints (Vent (V2 0 0) (V2 0 9))
 -- [V2 0 0,V2 0 1,V2 0 2,V2 0 3,V2 0 4,V2 0 5,V2 0 6,V2 0 7,V2 0 8,V2 0 9]
+-- >>> ventPoints (Vent (V2 0 0) (V2 1 1))
+-- >>> ventPoints (Vent (V2 1 1) (V2 0 0))
+-- >>> ventPoints (Vent (V2 2 0) (V2 0 2))
+-- >>> ventPoints (Vent (V2 0 2) (V2 2 0))
+-- [V2 0 0,V2 1 1]
+-- [V2 0 0,V2 1 1]
+-- [V2 0 2,V2 1 1,V2 2 0]
+-- [V2 0 2,V2 1 1,V2 2 0]
 ventPoints :: Vent -> [V2 Int]
-ventPoints (Vent (V2 x1 y1) (V2 x2 y2)) 
-  | x1 == x2 = let [smallY, bigY] = sort [y1, y2] 
-                in [ V2 x1 y | y <- [smallY .. bigY]]
-  | y1 == y2 = let [smallX, bigX] = sort [x1, x2] 
-                in [ V2 x y1 | x <- [smallX .. bigX]]
-  | otherwise = error "got diagonal vent!"
- 
+ventPoints (Vent u v)
+  | lx == rx = let [smallY, bigY] = sort [ly, ry]
+                in [ V2 lx y | y <- [smallY .. bigY]]
+  | ly == ry = [ V2 x ly | x <- [lx .. rx]]
+  | ly <= ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 1   ) left
+  | ly >  ry = takeWhile (`notRightOf` rx) $ iterate (+ V2 1 (-1)) left
+  | otherwise = error "vent is not vertical, horizontal, or 45 degree diagonal"
+  where
+    [left@(V2 lx ly), V2 rx ry] = sort [u,v]
+
+    notRightOf (V2 x _) r = x <= r
+
 
 -- >>> ventPointCounts (Vent (V2 1 0) (V2 1 5))
 -- >>> ventPointCounts (Vent (V2 0 0) (V2 0 9))
@@ -76,6 +95,7 @@ ventPointCounts = foldMap pcSingleton . ventPoints
 testInput :: IO Text
 testInput = do
   readFileText "inputs/day05example.txt"
+
 
 testVents :: IO [Vent]
 testVents = do
