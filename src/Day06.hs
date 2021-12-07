d674feab7bd6b98fcc31329dfd7d9a2b2cc54b3f
James Sully <sullyj3@gmail.com>
2021-12-07 14:25:24 +1100

    Cleanup

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,6 +1,5 @@
 module Day06 where
 
-
 import Text.Megaparsec
 import Text.Megaparsec.Char (space)
 
@@ -11,20 +10,16 @@ import Parsing
 import Data.List ((!!))
 import Utils
 
-type Fish = Int
 type FishCounter = IntMultiSet
 
-parseInput :: Parser [Fish]
-parseInput = commaSeparatedInts <* space <* eof
+parseInput :: Parser FishCounter
+parseInput = IMS.fromList <$> commaSeparatedInts <* space <* eof
 
 solve :: Text -> Text
 solve input = showSolutions p1 p2
-  where Just fishTimers = parseMaybe parseInput input
+  where Just initialCounts = parseMaybe parseInput input
         p1 = IMS.size $ iterate elapseDay initialCounts !! 80
         p2 = IMS.size $ iterate elapseDay initialCounts !! 256
-
-        initialCounts :: FishCounter
-        initialCounts = IMS.fromList fishTimers
 
 elapseDay :: FishCounter -> FishCounter
 elapseDay = IMS.concatMap \case
