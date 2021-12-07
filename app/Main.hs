54db8e0685cd6f2f19993b531465042ab3e86980
James Sully <sullyj3@gmail.com>
2021-12-07 13:20:02 +1100

    Allow running multiple days from main

--- app/Main.hs
+++ app/Main.hs
@@ -6,27 +6,36 @@ import qualified Day03
 import qualified Day04
 import qualified Day05
 import qualified Day06
+import Data.List ((!!))
 
 currentDay :: Int
 currentDay = 6
 
-getDay :: IO Int
-getDay =
+parseArgs :: IO ToRun
+parseArgs =
   getArgs <&> \case
-    [] -> currentDay
-    [ns] -> readMaybe ns ?: error "Expecting the argument to be a day number!"
-    _ -> error "Expecting 0 or 1 arguments for the day to execute"
+    [] -> Days [currentDay]
+    ["all"] -> AllDays
+    ns -> Days $ 
+      traverse readMaybe ns ?: error "Expecting the argument to be a day number, or 'all'!"
+
+data ToRun = Days [Int] | AllDays
 
 main :: IO ()
 main = do
-  day <- getDay
-  let (solve, input) = case day of
-        1 -> (Day01.solve, "day01.txt")
-        2 -> (Day02.solve, "day02.txt")
-        3 -> (Day03.solve, "day03.txt")
-        4 -> (Day04.solve, "day04.txt")
-        5 -> (Day05.solve, "day05.txt")
-        6 -> (Day06.solve, "day06.txt")
-  putTextLn $ "Day " <> show day <> ":"
-  putTextLn . solve =<< readFileText ("inputs/" ++ input)
+  toRun <- parseArgs
+  let days = [ (Day01.solve, "day01.txt")
+             , (Day02.solve, "day02.txt")
+             , (Day03.solve, "day03.txt")
+             , (Day04.solve, "day04.txt")
+             , (Day05.solve, "day05.txt")
+             , (Day06.solve, "day06.txt") ]
 
+      daysToRun :: [(Int, (Text -> Text, FilePath))]
+      daysToRun = case toRun of
+        AllDays -> [1..] `zip` days
+        Days ns -> map (\dayNum -> (dayNum, days !! (dayNum - 1))) ns
+
+  for_ daysToRun $ \(day, (solve, inputFP)) -> do
+    putTextLn $ "Day " <> show day <> ":"
+    putTextLn . solve =<< readFileText ("inputs/" ++ inputFP)
