e36ec2ca101b4909c8e4aab68b59317247cb3a0f
James Sully <sullyj3@gmail.com>
2021-12-07 17:16:26 +1100

    Formatting day7

--- src/Day07.hs
+++ src/Day07.hs
@@ -1,32 +1,34 @@
 module Day07 where
 
-import Utils (showSolutions)
-import Text.Megaparsec
-import Parsing
-import Text.Megaparsec.Char (space)
-import Data.List ((!!), minimum, maximum)
+import Data.List (maximum, minimum, (!!))
 import Data.Semigroup (Arg (Arg))
+import Parsing
+import Text.Megaparsec
+import Text.Megaparsec.Char (space)
+import Utils (showSolutions)
 
 parseInput :: Parser [Int]
 parseInput = commaSeparatedInts <* space <* eof
 
 solve :: Text -> Text
 solve input = showSolutions p1 p2
-  where Just crabs = parseMaybe parseInput input
-        p1 = part1 crabs
-        p2 = part2 crabs
+  where
+    Just crabs = parseMaybe parseInput input
+    p1 = part1 crabs
+    p2 = part2 crabs
 
 median :: Ord a => [a] -> a
 median ns = sort ns !! (length ns `div` 2)
 
 part1 :: [Int] -> Int
-part1 crabs = sum [abs (crab-med) | crab <- crabs]
-  where med = median crabs
+part1 crabs = sum [abs (crab - med) | crab <- crabs]
+  where
+    med = median crabs
 
 -- >>> map tri [0..10]
 -- [0,1,3,6,10,15,21,28,36,45,55]
 tri :: Int -> Int
-tri n = n * (n+1) `div` 2
+tri n = n * (n + 1) `div` 2
 
 -- >>> argMin (\x -> x * x) [-10..10]
 -- 0
@@ -34,14 +36,13 @@ tri n = n * (n+1) `div` 2
 -- -10
 argMin :: Ord b => (a -> b) -> [a] -> a
 argMin f candidates = theMin
-  where Arg _ theMin = minimum [ Arg (f x) x | x <- candidates]
+  where
+    Arg _ theMin = minimum [Arg (f x) x | x <- candidates]
 
--- >>> part2 [16,1,2,0,4,2,7,1,2,14]
 part2 :: [Int] -> Int
 part2 crabs = totalFuelCost
-  where 
+  where
     totalFuelCost = sum [movementCost target crab | crab <- crabs]
     (left, right) = (minimum crabs, maximum crabs)
-    target = argMin (\pos -> sum [movementCost pos crab | crab <- crabs]) [left..right]
+    target = argMin (\pos -> sum [movementCost pos crab | crab <- crabs]) [left .. right]
     movementCost pos crab = tri (abs $ pos - crab)
-
