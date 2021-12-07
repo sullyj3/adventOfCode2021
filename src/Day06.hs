a37fa040d0ff0ec8e36cac60513ae2241fb5cc3d
James Sully <sullyj3@gmail.com>
2021-12-07 14:21:05 +1100

    Simplify day 6 using multisets

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,17 +1,18 @@
 module Day06 where
 
-import Utils
 
 import Text.Megaparsec
 import Text.Megaparsec.Char (space)
 
-import Data.Map qualified as Map
-import Relude.Extra.Map
+import Data.IntMultiSet qualified as IMS
+import Data.IntMultiSet (IntMultiSet)
+
 import Parsing
+import Data.List ((!!))
+import Utils
 
 type Fish = Int
-
-type FishCounter = Map Fish Int
+type FishCounter = IntMultiSet
 
 parseInput :: Parser [Fish]
 parseInput = commaSeparatedInts <* space <* eof
@@ -19,25 +20,13 @@ parseInput = commaSeparatedInts <* space <* eof
 solve :: Text -> Text
 solve input = showSolutions p1 p2
   where Just fishTimers = parseMaybe parseInput input
-        p1 = simulateFish fishTimers 80
-        p2 = simulateFish fishTimers 256
+        p1 = IMS.size $ iterate elapseDay initialCounts !! 80
+        p2 = IMS.size $ iterate elapseDay initialCounts !! 256
 
-countFish :: [Fish] -> FishCounter
-countFish = Map.fromListWith (+) . (`zip` repeat 1)
-
-simulateFish :: [Fish] -> Int -> Int
-simulateFish initialFish days =
-  sum . elapseNDays days . countFish $ initialFish
+        initialCounts :: FishCounter
+        initialCounts = IMS.fromList fishTimers
 
 elapseDay :: FishCounter -> FishCounter
-elapseDay counter = Map.unionWith (+) (Map.mapKeysWith (+) ageFish counter) babies
-  where
-    ageFish :: Fish -> Fish
-    ageFish n | n == 0 = 6
-              | otherwise = n - 1
-
-    nZeros = lookupDefault 0 0 counter
-    babies = Map.singleton 8 nZeros
-
-elapseNDays :: Int -> FishCounter -> FishCounter
-elapseNDays n = appEndo . stimes n . Endo $ elapseDay
+elapseDay = IMS.concatMap \case
+  0 -> [6,8]
+  n -> [n-1]
