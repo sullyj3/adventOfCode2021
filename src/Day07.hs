794fe5867fe58d21f06d0781e2a60c9c933c129c
James Sully <sullyj3@gmail.com>
2021-12-07 17:37:34 +1100

    Simplify day 7

--- src/Day07.hs
+++ src/Day07.hs
@@ -6,6 +6,7 @@ import Parsing (Parser, commaSeparatedInts)
 import Text.Megaparsec (MonadParsec (eof), parseMaybe)
 import Text.Megaparsec.Char (space)
 import Utils (showSolutions)
+import Linear.V2 (V2(..))
 
 parseInput :: Parser [Int]
 parseInput = commaSeparatedInts <* space <* eof
@@ -14,29 +15,23 @@ solve :: Text -> Text
 solve input = showSolutions p1 p2
   where
     Just crabs = parseMaybe parseInput input
-    p1 = part1 crabs
-    p2 = part2 crabs
+
+    V2 p1 p2 = sum [ V2 (movementCostp1 crab)
+                        (movementCostp2 crab) | crab <- crabs ]
+
+    movementCostp1 crab =       abs $ crab - targetp1
+    movementCostp2 crab = tri . abs $ crab - targetp2
+
+    targetp1 = median crabs
+    targetp2 = mean crabs
 
 median :: Ord a => [a] -> a
 median ns = sort ns !! (length ns `div` 2)
 
 mean :: [Int] -> Int
 mean ns = sum ns `div` length ns
---
+
 -- >>> map tri [0..10]
 -- [0,1,3,6,10,15,21,28,36,45,55]
 tri :: Int -> Int
 tri n = n * (n + 1) `div` 2
-
-part1 :: [Int] -> Int
-part1 crabs = sum [abs (crab - target) | crab <- crabs]
-  where
-    target = median crabs
-
-part2 :: [Int] -> Int
-part2 crabs = sum [movementCost target crab | crab <- crabs]
-  where
-    target = mean crabs
-    movementCost pos crab = tri (abs $ pos - crab)
-
-
