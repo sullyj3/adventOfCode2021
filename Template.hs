946d8dc8f9392cc83ee86efdfe9bea63d2399e3e
James Sully <sullyj3@gmail.com>
2021-12-07 13:04:45 +1100

    Tweak template

--- Template.hs
+++ Template.hs
@@ -1,12 +1,19 @@
 module Day0X where
 
 import Utils (showSolutions)
+import Parsing
 
-parse :: Text -> Maybe ()
-parse = undefined
+parseInput :: Parser ()
+parseInput = pure ()
 
 solve :: Text -> Text
 solve input = showSolutions p1 p2
-  where Just () = parse input
-        p1 = ()
-        p2 = ()
+  where Just parsed = parseMaybe parseInput input
+        p1 = part1 parsed
+        p2 = part2 parsed
+
+part1 :: () -> ()
+part1 = undefined
+
+part2 :: () -> ()
+part2 = undefined
