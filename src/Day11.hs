f7a6ff6d05eb22f40a6003660cda141b34cc89a7
James Sully <sullyj3@gmail.com>
2021-12-11 23:36:15 +1100

    Use Vectors

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,17 +1,22 @@
 module Day11 where
 
 import Utils (showSolutions)
-import Text.Megaparsec
-import Parsing
 import Data.Text qualified as T
 import Data.Char (digitToInt)
 import Flow
+import Data.Vector qualified as V
+import Data.Vector (Vector)
+import Data.Vector.Mutable qualified as MV
+import Data.Vector.Mutable (MVector)
 
 type EnergyLevel = Int
+type Grid = Vector (Vector EnergyLevel)
+type MGrid s = MVector s (MVector s EnergyLevel)
 
 -- todo maybe vector? decide after reading problem
-parseInput :: Text -> [[EnergyLevel]]
-parseInput = T.lines .> map T.unpack .> map (map digitToInt)
+parseInput :: Text -> Grid
+parseInput = T.lines .> map T.unpack .> map (map digitToInt) .>
+  map V.fromList .> V.fromList
 
 solve :: Text -> Text
 solve input = showSolutions octopi p2
