95ba5ebb8ea3328ffa394cf7a0e9081b9b4335ab
James Sully <sullyj3@gmail.com>
2021-12-12 07:58:20 +1100

    day 11 switch more stuff to massiv

--- src/Day11.hs
+++ src/Day11.hs
@@ -13,16 +13,16 @@ import Data.List (delete)
 import Control.Monad.ST
 import Data.Massiv.Core
 import Data.Massiv.Array qualified as A
-import Data.Massiv.Array (U)
+import Data.Massiv.Array (U, D, Ix2(..))
+import Data.Massiv.Array.Mutable qualified as MA
 
 type EnergyLevel = Int
 
-type Grid = Matrix U Int
-type MGrid s = MMatrix s U Int
-type Point = V2 Int
+type Grid r = Matrix r Int
+type MGrid s r = MMatrix s r Int
 
 -- todo maybe vector? decide after reading problem
-parseInput :: Text -> Maybe Grid
+parseInput :: Text -> Maybe (Grid U)
 parseInput = T.lines .> map T.unpack .> map (map digitToInt) .> A.fromListsM Seq
 
 solve :: Text -> Text
@@ -31,27 +31,36 @@ solve input = showSolutions octopi p2
         p1 = ()
         p2 = ()
 
-{-
-adjacent :: Point -> [Point]
-adjacent pt@(V2 x y) =
-  delete pt [V2 (x+dx) (y+dy) | dx <- [-1..1], dy <- [-1..1]]
+adjacent :: Ix2 -> [Ix2]
+adjacent pt@(Ix2 x y) =
+  delete pt [(x+dx) :. (y+dy) | dx <- [-1..1], dy <- [-1..1]]
 
-inBounds :: Point -> Bool
-inBounds (V2 x y) = 0 <= x && x < width && 0 <= y && y < height
+inBounds :: Ix2 -> Bool
+inBounds (x :. y) = 0 <= x && x < width && 0 <= y && y < height
   where (width, height) = (10, 10)
 
+
+increaseEnergy :: Grid U -> Grid D
+increaseEnergy = A.map succ
+
+
+--
+-- findGTNines :: MGrid s r -> [Ix2]
+-- findGTNines = gridIMapMaybe 
+--   (\pt energy -> guard (energy > 9) >> pure pt)
+
+{-
 step :: Grid -> Grid
 step = increaseEnergy .> flash
   where
-    increaseEnergy = V.map (V.map succ) g
 
 flash grid = runST do
   mgrid <- thaw grid
   let loop = do
         let toFlash = findGTNines mv
         when (not . null $ toFlash) do
-          for_ toFlash \flashPoint -> do
-            let adjacents = filter inBounds (adjacent flashPoint)
+          for_ toFlash \flashIx2 -> do
+            let adjacents = filter inBounds (adjacent flashIx2)
             for_ adjacents (modifyGrid mgrid succ)
           loop
   loop
@@ -60,11 +69,8 @@ flash grid = runST do
 modifyGrid mgrid f (V2 x y) =
   MV.modify mgrid (\row -> Mv.modify row f x) y
 
-findGTNines :: MGrid s -> [Point]
-findGTNines = gridIMapMaybe 
-  (\pt energy -> guard (energy > 9) >> pure pt)
 
-gridIMapMaybe :: (Point -> a -> Maybe b) -> Grid -> [b]
+gridIMapMaybe :: (Ix2 -> a -> Maybe b) -> Grid -> [b]
 gridIMapMaybe f = concat . V.toList . V.imap 
   (\y row -> V.imapMaybe (\x -> f (V2 x y)))
 
