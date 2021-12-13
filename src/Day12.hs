6a808b9049951a0439661e0e86fbd30eae024426
James Sully <sullyj3@gmail.com>
2021-12-13 15:10:33 +1100

    Day 12 part 1

--- src/Day12.hs
+++ src/Day12.hs
@@ -10,27 +10,30 @@ import Data.Set qualified as Set
 import Prelude hiding (some)
 import Text.Megaparsec.Char (string, lowerChar, upperChar, space)
 import qualified Data.Text as T
+import Relude.Extra.Map ((!?))
 
 
-data Node = Start | Little Text | Big Text | End
+data Node = Start | Small Text | Big Text | End
   deriving (Show, Eq, Ord)
 type Edge = (Node, Node)
 type Graph = Map Node (Set Node)
 
-node :: Parser Node
-node = try (Start <$ string "start") <|>
+parseNode :: Parser Node
+parseNode = try (Start <$ string "start") <|>
        try (End <$ string "end") <|>
-       try (Little . T.pack <$> some lowerChar ) <|>
-       (Big . T.pack <$> some upperChar)
+       try (Small . T.pack <$> some lowerChar ) <|>
+       Big . T.pack <$> some upperChar
 
 edge :: Parser Edge
-edge = (,) <$> (node <* single '-') <*> node
+edge = (,) <$> (parseNode <* single '-') <*> parseNode
 
 edges :: Parser [Edge]
 edges = edge `sepEndBy` space
 
 graphFromEdges :: [Edge] -> Graph
-graphFromEdges = Map.fromListWith (<>) . map (second Set.singleton)
+graphFromEdges = Map.fromListWith (<>) . map (second Set.singleton) . addReverseEdges
+  where addReverseEdges es = es <> map swap es
+
 
 exampleGraph :: IO Graph
 exampleGraph = do
@@ -40,10 +43,10 @@ exampleGraph = do
 
 
 solve :: Text -> Text
-solve input = showSolutions graph ()
+solve input = showSolutions p1 ()
   where Just theEdges = parseMaybe edges input
         graph = graphFromEdges theEdges
-        -- p1 = part1 ()
+        p1 = numPaths graph
         -- p2 = part2 ()
 
 -- part1 :: () -> ()
@@ -51,3 +54,21 @@ solve input = showSolutions graph ()
 
 -- part2 :: () -> ()
 -- part2 = undefined
+type VisitedSet = Set Node
+
+numPaths :: Graph -> Int
+numPaths graph = length $ flip evalStateT mempty $ go Start
+  where
+    go :: Node -> StateT VisitedSet [] ()
+    go = \case
+      End -> pure ()
+      node -> do
+        visited :: VisitedSet <- get
+        guard $ node `Set.notMember` visited
+        case node of
+          Small _ -> modify (Set.insert node)
+          Start -> modify (Set.insert node)
+          _ -> pass
+
+        neighbour <- lift $ Set.toList $ (graph !? node) ?: mempty
+        go neighbour
