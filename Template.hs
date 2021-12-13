a50841573c8aa22412dedca442632760916dfb46
James Sully <sullyj3@gmail.com>
2021-12-13 13:21:57 +1100

    Tweak template

--- Template.hs
+++ Template.hs
@@ -1,4 +1,4 @@
-module Day0X where
+module DayX where
 
 import Utils (showSolutions)
 import Text.Megaparsec
@@ -8,13 +8,13 @@ parseInput :: Parser ()
 parseInput = pure ()
 
 solve :: Text -> Text
-solve input = showSolutions p1 p2
+solve input = showSolutions () ()
   where Just parsed = parseMaybe parseInput input
-        p1 = part1 parsed
-        p2 = part2 parsed
+        -- p1 = part1 parsed
+        -- p2 = part2 parsed
 
-part1 :: () -> ()
-part1 = undefined
+-- part1 :: () -> ()
+-- part1 = undefined
 
-part2 :: () -> ()
-part2 = undefined
+-- part2 :: () -> ()
+-- part2 = undefined
